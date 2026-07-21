d85f1381a3eb548504232108568cb8936edb4518
DESKTOP-V63BQ7C\Admin <blackbean9081@gmail.com>
2026-07-21 10:27:13 +0900

    etc : 기초 세팅

---
+++ tably/src/main/java/com/app/tably/TablyApplication.java
@@ -0,0 +1,13 @@
+package com.app.tably;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablyApplication.class, args);
+	}
+
+}
